4804b199f35b73db3bbfa0f1c14920643ed26ab2
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-01-20 20:08:38 +0100

    Add new feature in task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+/************ fun RandomHexColor ************/
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+// console.log(getRandomHexColor());
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
+let int;
+buttonStop.disabled = true;
+
+/************ start  ************/
+buttonStart.addEventListener('click', handlerClickOnStart);
+function handlerClickOnStart() {
+  int = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+}
+
+/************ stop ************/
+buttonStop.addEventListener('click', handlerClickOnStop);
+function handlerClickOnStop() {
+  clearInterval(int);
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+}
